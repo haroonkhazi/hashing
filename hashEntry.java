87325998e1f855df2df90dcad96da14e3819263d
Haroon Khazi <haroonkhazi@gmail.com>
2017-04-03 14:18:19 -0400

    updated comments

--- hashEntry.java
+++ hashEntry.java
@@ -6,15 +6,21 @@ import java.util.ArrayList;
 
 public class hashEntry{
     
-    
+    //hash #
     int hash;
+    //key of hashEntry
     Object key;
+    //word of the entry
     String value;
+    //the next hashentry after this
     hashEntry next;
+    //number of times this word has come up
     int count;
+    /**
+     * @return String value containing word of entry and the count
+     */
     @Override
     public String toString(){
-//System.out.println("toString");
         String st = value + " " + count;
         return st;
     }
